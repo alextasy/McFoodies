b37e5bfb627947390aea02afe998c8226d3d07d1
alextasy <alex05tmm@gmail.com>
2020-11-02 20:08:25 +0200

    Created carousel modal animations and different descriptions for each image.

--- src/components/regular_components/hero/Hero.js
+++ src/components/regular_components/hero/Hero.js
@@ -8,6 +8,7 @@ import img5 from '../../../images/main/5.jpg';
 import img6 from '../../../images/main/6.jpg';
 import shadow from '../../../images/main/Shadow.png';
 import Button from '../button/Button';
+import heroDescriptions from './card_descriptions';
 
 function Hero() {
 
@@ -20,15 +21,18 @@ function Hero() {
         const indicators = [...button.parentNode.lastChild.children];
         const firstImgCloneIndex = arrayOfImg.length - 1;// It's the last img of the array. -1 because of the clone at the start
         const lastImgCloneIndex = 0;
+        const modal = button.parentNode.children[4];
         
         
         indicators.forEach((element)=>{
             element.style.transitionDuration = transitionLenght + 'ms';
             element.style.animationDuration = transitionLenght + 'ms';
         })
-        
         indicators[counter-1].className = ''; // Previous image indicator becomes inactive
+
         counter += direction; // direction = +1 or -1
+        modal.style.animation = `flip ${transitionLenght}ms ease-in-out ${direction === -1 ? 'alternate' : ''}`;
+        setTimeout(()=> updateModalText(modal), transitionLenght/2);
 
         switch(counter) //Assignes the active class to the proper element
         { 
@@ -38,20 +42,23 @@ function Hero() {
         };
 
         arrayOfImg.forEach((element)=> {
-            element.addEventListener('transitionend', ()=> {element.style.animation = 'none'}); // Resets animation, so it can be played again; 
+            element.addEventListener('transitionend', ()=> {
+                element.style.animation = 'none';  // Resets animation, so it can be played again;
+                modal.style.animation = 'none';
+            });  
 
             element.style.transition = `transform ease-in-out ${transitionLenght}ms`;
-            element.style.animation = `slide ${transitionLenght}ms`;
+            element.style.animation = `fade ${transitionLenght}ms ease-in-out`;
             element.style.transform = `translateX(-${100*counter}%)`;
         });
 
         if(counter === lastImgCloneIndex || counter === firstImgCloneIndex){
-            setTimeout(() => resetSlides(arrayOfImg, firstImgCloneIndex), transitionLenght); // Waiting for transition end
+            setTimeout(() => resetSlides(arrayOfImg, firstImgCloneIndex, modal), transitionLenght); // Waiting for transition end
         }
 
     }
 
-    const resetSlides = (arrayOfImg, firstImgCloneIndex) =>{
+    const resetSlides = (arrayOfImg, firstImgCloneIndex, modal) =>{
 
         const firstImgIndex = 1;
         const lastImgIndex = firstImgCloneIndex -1; // The clone is the last in the array, actual last is the one before it
@@ -61,6 +68,7 @@ function Hero() {
         arrayOfImg.forEach((element)=> {
             element.style.transition = 'none';
             element.style.transform = `translateX(-${100*index}%)`;
+            modal.style.animation = 'none';
         });
         counter = index;
     }
@@ -68,6 +76,22 @@ function Hero() {
     const temporaryDisableButton = (button)=>{
         button.disabled = true;
         setTimeout(()=> button.disabled = false, transitionLenght)
+    }
+
+    const updateModalText = (modal)=>{
+        const h1 = modal.firstChild;
+        const p = modal.children[1];
+
+        switch(counter){
+            case 0: h1.innerHTML = heroDescriptions.hero6.h1; p.innerHTML = heroDescriptions.hero6.p; break;
+            case 1: h1.innerHTML = heroDescriptions.hero1.h1; p.innerHTML = heroDescriptions.hero1.p; break;
+            case 2: h1.innerHTML = heroDescriptions.hero2.h1; p.innerHTML = heroDescriptions.hero2.p; break;
+            case 3: h1.innerHTML = heroDescriptions.hero3.h1; p.innerHTML = heroDescriptions.hero3.p; break;
+            case 4: h1.innerHTML = heroDescriptions.hero4.h1; p.innerHTML = heroDescriptions.hero4.p; break;
+            case 5: h1.innerHTML = heroDescriptions.hero5.h1; p.innerHTML = heroDescriptions.hero5.p; break;
+            case 6: h1.innerHTML = heroDescriptions.hero6.h1; p.innerHTML = heroDescriptions.hero6.p; break;
+            case 7: h1.innerHTML = heroDescriptions.hero1.h1; p.innerHTML = heroDescriptions.hero1.p; break;   
+        }     
     }
 
 
@@ -98,8 +122,8 @@ function Hero() {
 
             <div className="order_modal">
                 <h1>FEELING HUNGRY?</h1>
-                <p>Delicous food at your doorstep in no time. High quality fresh ingredients for high quality people.</p>
-                <Button>ORDER NOW</Button>
+                <p>Delicous food at your doorstep in no time. High quality fresh ingredients <br/>for high quality people.</p>
+                <Button style={{margin: '0 auto'}}>ORDER NOW</Button>
             </div>
 
             <div className="indicators">
---
+++ src/components/regular_components/hero/card_descriptions.js
@@ -0,0 +1,27 @@
+const hero = {
+    hero1 : {
+        h1: 'FEELING HUNGRY?',
+        p:  'Delicous food at your doorstep in no time. High quality fresh ingredients <br/>for high quality people.'
+    },
+    hero2 : {
+        h1: 'FREE DELIVERY',
+        p:  'Free delivery on all orders above $9.99 Get your delicous food without any extra costs!'
+    },
+    hero3 : {
+        h1: 'FAST & EASY',
+        p:  'Only a few clicks away from having your food at your door in the next </br> 35 minutes.'
+    },
+    hero4 : {
+        h1: 'FRESHLY COOKED',
+        p:  'Everythin is cooked on demand. There is nothing like the taste of food that finished cooking just now'
+    },
+    hero5 : {
+        h1: 'DESERTS?',
+        p:  'Craving something sweet? No problem, we got you covered with our freshly baked deserts and ice cream selection!'
+    },
+    hero6 : {
+        h1: 'ORDER NOW!',
+        p:  'Choose your meal from all of our great options and have it delivered to you now!'
+    }
+}
+export default hero;

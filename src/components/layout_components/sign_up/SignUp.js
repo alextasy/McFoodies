103c6bec66fcef2f59996877ad8b8248a2896b33
alextasy <alex05tmm@gmail.com>
2021-01-28 01:08:48 +0200

    Fixed a bug where the wrong cities where displayed as options in SignUp.js

--- src/components/layout_components/sign_up/SignUp.js
+++ src/components/layout_components/sign_up/SignUp.js
@@ -11,8 +11,8 @@ import { AuthContext } from '../../../context/AuthContext';
 function SignUp(props) {
 
     const [userInfo, setUserInfo] = useState({
-        email: '', 
-        password: '', 
+        email: '',
+        password: '',
         repeatPassword: '',
         name: '',
         firstLine: '',
@@ -29,10 +29,10 @@ function SignUp(props) {
 
     //Acurately tracks if passwords match and updates based on that
 
-    useEffect(()=>{ 
+    useEffect(()=>{
         if(userInfo.password === userInfo.repeatPassword) {
             document.querySelector('#repeatPassword').parentNode.classList.remove('invalid_password');
-            return; 
+            return;
         }
     }, [userInfo.password, userInfo.repeatPassword]);
 
@@ -40,14 +40,14 @@ function SignUp(props) {
         const id = element.id;
         const value = element.value;
         const parentDiv = element.parentNode;
-        
-        element.placeholder = 'This field is required...';
-        value === '' ? parentDiv.classList.add('empty') : parentDiv.classList.remove('empty');            
 
-        (element.minLength !== -1 && value.length < element.minLength) 
+        element.placeholder = 'This field is required...';
+        value === '' ? parentDiv.classList.add('empty') : parentDiv.classList.remove('empty');
+
+        (element.minLength !== -1 && value.length < element.minLength)
             ? parentDiv.classList.add('invalid')
             : parentDiv.classList.remove('invalid');
-        
+
         setUserInfo({...userInfo, [id]: value});
     }
 
@@ -83,14 +83,14 @@ function SignUp(props) {
 
         // Removes passwords that shouldn't be stored
         const noPassword = ({password, repeatPassword, ...rest}) => rest;
-        const newInfo = noPassword(userInfo); 
+        const newInfo = noPassword(userInfo);
 
         setIsModalOpen(true);
 
         firebaseAuth.createUserWithEmailAndPassword(userInfo.email, userInfo.password)
             .then((credentials)=>{
                 const userId = credentials.user.uid;
-                
+
                 //Creates a user doc matching the Auth UID
 
                 db.collection('users').doc(userId).set( {
@@ -99,11 +99,11 @@ function SignUp(props) {
 
 
                 //Changes the global state/context to be auth
-               
+
                 authContext.setUserInfo({...newInfo});
                 authContext.setUserID(userId);
                 authContext.setIsAuth(true);
-                   
+
                 setIsSuccessful(true);
                 setSignUpMessage('Thank you for signing up. Enjoy our delicious food!');
 
@@ -121,14 +121,14 @@ function SignUp(props) {
         if(isSuccessful) props.history.push('/menu');
         closeModal(()=> setIsModalOpen(false));
     }
-    
-    const signUpModal = 
+
+    const signUpModal =
         <Modal click={closeOrRedirect}>
             {signUpMessage ? <p>{signUpMessage}</p> : <Spinner small={true}/>}
 
             <Button click={closeOrRedirect}>CLOSE</Button>
         </Modal>
- 
+
 
     return (
         <Container class='Sign_up'>
@@ -137,11 +137,11 @@ function SignUp(props) {
 
                 <div className='input_div'>
                     <label>EMAIL:</label>
-                    <input 
-                        type='email' 
-                        id='email' 
+                    <input
+                        type='email'
+                        id='email'
                         value={userInfo.email}
-                        maxLength='25' 
+                        maxLength='25'
                         onChange={(e) => {
                             updateState(e.target);
                             e.target.parentNode.classList.remove('invalid_email'); //resets if form was submited with invalid class
@@ -151,26 +151,26 @@ function SignUp(props) {
 
                 <div className='input_div'>
                     <label>PASSWORD:</label>
-                    <input 
+                    <input
                         type='password'
-                        id='password' 
+                        id='password'
                         value={userInfo.password}
-                        minLength='7' 
-                        maxLength='25' 
-                        onChange={(e) => updateState(e.target)} 
+                        minLength='7'
+                        maxLength='25'
+                        onChange={(e) => updateState(e.target)}
                     />
                 </div>
 
                 <div className='input_div'>
                     <label>REPEAT PASSWORD:</label>
-                    <input 
+                    <input
                         type='password'
-                        id='repeatPassword' 
-                        value={userInfo.repeatPassword} 
-                        maxLength='25' 
+                        id='repeatPassword'
+                        value={userInfo.repeatPassword}
+                        maxLength='25'
                         onChange={(e) => {
                             updateState(e.target);
-                            e.target.parentNode.classList.add('invalid_password');  
+                            e.target.parentNode.classList.add('invalid_password');
                         }} />
                 </div>
 
@@ -178,24 +178,24 @@ function SignUp(props) {
 
                 <div className='input_div'>
                     <label>FULL NAME:</label>
-                    <input 
+                    <input
                         type='text'
                         id='name'
-                        value={userInfo.name} maxLength='30'  
+                        value={userInfo.name} maxLength='30'
                         onChange={(e) => updateState(e.target)} />
                 </div>
 
                 <div className='input_div'>
                     <label>PHONE NUMBER:</label>
-                    <input 
+                    <input
                         type='text'
                         id='phoneNumber'
                         value={userInfo.phoneNumber} maxLength='15'
-                        minLength='7' 
+                        minLength='7'
                         onChange={(e) => updateState(e.target)} />
                 </div>
 
-                
+
             </form>
 
             <form>
@@ -203,54 +203,54 @@ function SignUp(props) {
 
                 <div className='input_div'>
                     <label>FIRST LINE OF ADDRESS:</label>
-                    <input 
+                    <input
                         type='text'
-                        id='firstLine' 
+                        id='firstLine'
                         value={userInfo.firstLine}
-                        maxLength='35' 
+                        maxLength='35'
                         onChange={(e) => updateState(e.target)} />
                 </div>
 
                 <div className='input_div'>
                     <label>SECOND LINE OF ADDRESS (OPTIONAL):</label>
-                    <input 
+                    <input
                         type='text'
                         id='secondLine'
-                        value={userInfo.secondLine} 
-                        maxLength='35' 
+                        value={userInfo.secondLine}
+                        maxLength='35'
                         onChange={(e) => setUserInfo({...userInfo, secondLine: e.target.value})} />
                 </div>
 
                 <div className='input_div' id='city'>
                     <label>CITY:</label>
-                    <select 
+                    <select
                         onChange={(e) => setUserInfo({...userInfo, city: e.target.value})}
                         onClick={(e)=> e.target.parentNode.classList.remove('empty') // If form was submited this class was added
                     }>
                             <option value=''> Choose city</option>
-                            <option value='New York'>London</option>
-                            <option value='Brooklyn'>Manchester</option>
-                            <option value='Bronx'>Swansea</option>
-                            <option value='Queens'>Bristol</option>
+                            <option value='New York'>New York</option>
+                            <option value='Brooklyn'>Brooklyn</option>
+                            <option value='Bronx'>Bronx</option>
+                            <option value='Queens'>Queens</option>
                     </select>
                 </div>
 
                 <div className='input_div'>
                     <label>ZIP/POSTAL CODE:</label>
-                    <input 
-                        type='text' 
-                        id='zipCode' 
+                    <input
+                        type='text'
+                        id='zipCode'
                         value={userInfo.zipCode}
-                        minLength='7' 
-                        maxLength='7' 
+                        minLength='7'
+                        maxLength='7'
                         onChange={(e) => updateState(e.target)} />
                 </div>
 
                 <div className='input_div checkbox'>
-                    <input 
+                    <input
                         type='checkbox'
-                        id='newsletter' 
-                        value={userInfo.newsletter} 
+                        id='newsletter'
+                        value={userInfo.newsletter}
                         onChange={(e) => setUserInfo({...userInfo, newsletter: e.target.checked})} />
                     <label>I want to sign up to McFoodie's newsletter to receive information about future products, promotions and discounts. </label>
                 </div>
